af4758b6c1f295ca01546d4baf5a4f0736580d81
Dishant <dishantd442442@gmail.com>
2024-05-12 15:04:11 +0530

    created the temp component for weather app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
+import "./App.css";
+import Temperature from "./compoents/Temperature";
 
 function App() {
-
   return (
     <>
-      <h1 className="">hello world</h1>
+      <div className="flex bg-[#1F213A] h-screen justify-center align-top">
+        <div className=" mt-40 w-1/5 h-1/3">
+          <Temperature />
+        </div>
+        <div className="mt-40 bg-red-400 w-1/3 h-1/3 p-8">
+          <h2 className="text-slate-200 text-2xl">Today's Highlights</h2>
+        </div>
+      </div>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
