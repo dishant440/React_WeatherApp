29365fbda6c62b665cd81c1808879ceb6ae4ff79
Dishant <dishantd442442@gmail.com>
2024-05-13 12:49:42 +0530

    improved the code readability

--- src/compoents/Highlights.jsx
+++ src/compoents/Highlights.jsx
@@ -13,54 +13,53 @@ export default function Highlights({ highlightStates }) {
           <span className="text-2xl pl-2">kph</span>
         </div>
       )}
-      {
-        highlightStates.humidity && (
-          <div className="mt-2">
+
+      {highlightStates.humidity && (
+        <div className="mt-2">
           <span className="text-4xl font-bold">
             {highlightStates.humidity}
           </span>
           <span className="text-2xl pl-2">{highlightStates.unit}</span>
         </div>
-        )
-      }
-      {
-        highlightStates.visibility && (
-          <div className="mt-2">
+      )}
+
+      {highlightStates.visibility && (
+        <div className="mt-2">
           <span className="text-4xl font-bold">
             {highlightStates.visibility}
           </span>
           <span className="text-2xl pl-2">{highlightStates.unit}</span>
         </div>
-        )
-      }
-      {
-        highlightStates.pressure && (
-          <div className="mt-2">
+      )}
+
+      {highlightStates.pressure && (
+        <div className="mt-2">
           <span className="text-4xl font-bold">
             {highlightStates.pressure}
           </span>
           <span className="text-2xl pl-2">{highlightStates.unit}</span>
         </div>
-        )
-      }
+      )}
 
-      {highlightStates.direction && (<div className="mt-2 flex">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6 text-slate-200"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
-          />
-        </svg>
-        <div className="ms-2 text-slate-200">{highlightStates.direction}</div>
-      </div>)}
+      {highlightStates.direction && (
+        <div className="mt-2 flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 text-slate-200 mr-2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+            />
+          </svg>
+          <div>{highlightStates.direction}</div>
+        </div>
+      )}
     </div>
   );
 }
